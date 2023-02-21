b9b03703a167de7caac47a58f445de36a6588ed3
igor konst <uropb.8@mail.ru>
2023-02-21 22:33:55 +0300

    sec commit

--- RockPaperScissors.sol
+++ RockPaperScissors.sol
@@ -11,25 +11,25 @@ contract RockPaperScissors{
     constructor() {
         owner = msg.sender;
     }
-    function playGame(uint8 _option) public payable{   
+    function playGame(uint8 _option) public payable{   //с фронта 0-rock 1-scissors 2-papper
         require(msg.value*2 <= address(this).balance, "no have money for you");
         uint choise;
         if (block.timestamp%2==0){
-            choise=0;
+            choise=0; //rock
         }else if (block.timestamp%3==0){
-            choise=1;
+            choise=1; //scissors
         }else{
-            choise=2;
+            choise=2; //papper
         }
 
         if ((choise == 0 && _option == 2) ||(choise == 1 && _option == 0) ||(choise == 2 && _option == 1)){
             payable(msg.sender).transfer(msg.value*2);
-            emit GameEnd(msg.sender, msg.value, _option, 0 );
+            emit GameEnd(msg.sender, msg.value, _option, 0 ); //победа
         } else if ((choise == 0 && _option == 1) ||(choise == 1 && _option == 2) || (choise == 2 && _option == 0)){
-            emit GameEnd(msg.sender, msg.value, _option, 1 );
+            emit GameEnd(msg.sender, msg.value, _option, 1 );// проигрыш
         }else{
             emit GameEnd(msg.sender, msg.value, _option, 2 );
-            payable(msg.sender).transfer(msg.value);
+            payable(msg.sender).transfer(msg.value); //ничья
         }
     }
     function withdraw() public onlyOwner {
